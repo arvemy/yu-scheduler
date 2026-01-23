e602a2750595940ebfd6cb75d4be6e17b05b279a
arvemy <22ardakorkmaz@gmail.com>
2026-01-23 21:32:55 +0300

    fix(ui): use DropdownMenu.Portal to ensure menu renders on top of all elements

--- src/lib/components/AppHeader.svelte
+++ src/lib/components/AppHeader.svelte
@@ -62,46 +62,48 @@
 						<path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
 					</svg>
 				</DropdownMenu.Trigger>
-				<DropdownMenu.Content class="menu" sideOffset={8} align="end">
-					<DropdownMenu.Item class="menu-item" onSelect={() => openExternal(portfolioUrl)}>
-						<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-							<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
-						</svg>
-						{$t('nav.portfolio')}
-					</DropdownMenu.Item>
-					<DropdownMenu.Item class="menu-item" onSelect={() => openExternal('https://github.com/arvemy/yu-scheduler')}>
-						<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-							<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-						</svg>
-						{$t('nav.github')}
-					</DropdownMenu.Item>
-					<DropdownMenu.Item class="menu-item" onSelect={() => openExternal('https://linkedin.com/in/2123ardakorkmaz')}>
-						<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-							<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-						</svg>
-						{$t('nav.linkedin')}
-					</DropdownMenu.Item>
-					<DropdownMenu.Separator class="menu-sep" />
-					<DropdownMenu.Item class="menu-item" onSelect={onOpenWelcome}>
-						<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-							<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
-						</svg>
-						{$t('nav.showWelcome')}
-					</DropdownMenu.Item>
-					<DropdownMenu.Separator class="menu-sep" />
-					<div class="menu-section-title">{$t('language.switchLanguage')}</div>
-					{#each languageItems as item (item.value)}
-						<DropdownMenu.Item 
-							class="menu-item {$locale === item.value ? 'selected' : ''}"
-							onSelect={() => handleLanguageChange(item.value)}
-						>
+				<DropdownMenu.Portal>
+					<DropdownMenu.Content class="menu" sideOffset={8} align="end">
+						<DropdownMenu.Item class="menu-item" onSelect={() => openExternal(portfolioUrl)}>
 							<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-								<path d="M12.87 15.07l-2.54-2.51.03-.03A17.52 17.52 0 0014.07 6H17V4h-7V2H8v2H1v2h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"/>
+								<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
 							</svg>
-							{item.label}
+							{$t('nav.portfolio')}
 						</DropdownMenu.Item>
-					{/each}
-				</DropdownMenu.Content>
+						<DropdownMenu.Item class="menu-item" onSelect={() => openExternal('https://github.com/arvemy/yu-scheduler')}>
+							<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+								<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+							</svg>
+							{$t('nav.github')}
+						</DropdownMenu.Item>
+						<DropdownMenu.Item class="menu-item" onSelect={() => openExternal('https://linkedin.com/in/2123ardakorkmaz')}>
+							<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+								<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+							</svg>
+							{$t('nav.linkedin')}
+						</DropdownMenu.Item>
+						<DropdownMenu.Separator class="menu-sep" />
+						<DropdownMenu.Item class="menu-item" onSelect={onOpenWelcome}>
+							<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+								<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
+							</svg>
+							{$t('nav.showWelcome')}
+						</DropdownMenu.Item>
+						<DropdownMenu.Separator class="menu-sep" />
+						<div class="menu-section-title">{$t('language.switchLanguage')}</div>
+						{#each languageItems as item (item.value)}
+							<DropdownMenu.Item 
+								class="menu-item {$locale === item.value ? 'selected' : ''}"
+								onSelect={() => handleLanguageChange(item.value)}
+							>
+								<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+									<path d="M12.87 15.07l-2.54-2.51.03-.03A17.52 17.52 0 0014.07 6H17V4h-7V2H8v2H1v2h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"/>
+								</svg>
+								{item.label}
+							</DropdownMenu.Item>
+						{/each}
+					</DropdownMenu.Content>
+				</DropdownMenu.Portal>
 			</DropdownMenu.Root>
 		{:else}
 			<!-- Desktop: individual buttons -->
